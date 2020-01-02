a4aac31941d5fe8d346e7c1d5afef9044ec2377b
Nguyễn Khắc Hoàn <hoanuchiha98@gmail.com>
2020-01-02 17:53:33 +0700

    bổ sung thuộc tính cho product

--- models/product.py
+++ models/product.py
@@ -6,6 +6,9 @@ class ProductModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, server_default=db.text("nextval('product_id_seq'::regclass)"))
     product_name = db.Column(db.String, nullable=False)
     product_price = db.Column(db.Float(53), nullable=False)
+    count = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    photo = db.Column(db.Text, nullable=False)
 
 class ProductSchema(ma.ModelSchema):
     class Meta:
@@ -14,7 +17,10 @@ class ProductSchema(ma.ModelSchema):
         fields = (
             'id',
             'product_name',
-            'product_price'
+            'product_price',
+            'count',
+            'description',
+            'photo'
         )
 
     def make(self, data, **kwargs):
