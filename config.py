75d52f121eaf895498d24d89496e62d5ba56b41e
hoanuchiha98 <42841647+hoanuchiha98@users.noreply.github.com>
2020-01-03 18:15:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ flask_app = connex_app.app
 flask_app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_CONNECTION_STR
 flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 flask_app.config["SQLALCHEMY_ECHO"] = True  # turn on sql query on logging
-# flask_app.config["CORS_HEADERS"] = 'Content-Type'
+flask_app.config["CORS_HEADERS"] = 'Content-Type'
 flask_app.config['SECRET_KEY'] = "QdjqxOEiclZ5OIM26yBxd6K5N4TgsW3UYNG0uc5hvn0IGrbgTeSW52wld0svkUjABJJ5rUL5VBSMGOpHpswhUqjJhaRR7W2MKyC1E4bvs8dE0Ft1dRlckaacpWZ9oBIuXPLYUZVeqpOMwR3dWr4SLkwt142hsSZxwR3dWr4SLkwt142hsSZx4bN8mIifIh7Uh0tGHk59"
 UPLOAD_FOLDER = './static/images'
 
